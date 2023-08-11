10e1fa14bf66367c231491ee2b0220b0dc811f29
Hrach333 <hrach@hrach.ru>
2023-08-11 10:40:58 +0300

    removed unnecessary comments

--- src/views/home.php
+++ src/views/home.php
@@ -4,9 +4,9 @@ require 'templates/head.php';
 
     <body>
 <form enctype="multipart/form-data" action="/upload" method="POST">
-    <!-- Поле MAX_FILE_SIZE должно быть указано до поля загрузки файла -->
+
     <input type="hidden" name="MAX_FILE_SIZE" value="10000" />
-    <!-- Название элемента input определяет имя в массиве $_FILES -->
+
     Отправить этот файл: <input name="file" type="file" />
     <input type="submit" value="Отправить файл" />
 </form>
